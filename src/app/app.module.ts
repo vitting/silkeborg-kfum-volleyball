384cf2c15cc9e45b3ffc2ab4ddf22f000852cbbb
Christian Nicolaisen <cvn_vitting@hotmail.com>
2020-02-08 21:27:45 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { AngularFireModule } from "@angular/fire";
+import { environment } from "../environments/environment";
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    ServiceWorkerModule.register("ngsw-worker.js", { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,19 @@
+import { firebaseEnvironment } from "./firebase-environment";
+
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: firebaseEnvironment.apiKey,
+    authDomain: firebaseEnvironment.authDomain,
+    databaseURL: firebaseEnvironment.databaseURL,
+    projectId: firebaseEnvironment.projectId,
+    storageBucket: firebaseEnvironment.storageBucket,
+    messagingSenderId: firebaseEnvironment.messagingSenderId
+  }
 };
 
 /*
